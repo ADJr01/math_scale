bd1222bc48c70e4b663ee030b90b2dd3fa87261d
Rahat Adnan <sthep90@gmail.com>
2026-01-22 13:53:36 +0600

    sample variability

--- maths/main.py
+++ maths/main.py
@@ -1,10 +1,14 @@
 import numpy as np
 
 if __name__ == '__main__':
-    nums = np.random.random(10)
-    max = np.max(nums)
-    min = np.min(nums)
-    max_index = np.argmax(nums)
-    min_index = np.argmin(nums)
-    print(f"Max is {max} at index {max_index}")
-    print(f"Min is {min} at index {min_index}")
+    dataset = [18,21,37,46,57,48,53,52,68,22,30,33,35,19,20,40,41,52,57,46,48,53,52,68,22,30,33,35,36,66,70,57,27,38,39,44,59,51,61,69]
+    sub_set_1 = np.random.choice(a=dataset,size=5,replace=True)
+    sub_set_2 = np.random.choice(a=dataset,size=5,replace=True)
+    mean_of_all_ages = np.mean(dataset)
+    mean_of_subset_1 = np.mean(sub_set_1)
+    mean_of_subset_2 = np.mean(sub_set_2)
+    print(f"Person with max age: {np.argmax(dataset)}")
+    print(f"Person with min age: {np.argmin(dataset)}")
+    print(f"Mean of All Ages: {mean_of_all_ages}\nMean of Subset1 {mean_of_subset_1}\nMean of Subset2 {mean_of_subset_2}")
+    print(np.random.choice(np.random.rand(1),size=5,replace=True))
+
